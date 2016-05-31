73ad9fc502323f882d6b696ab8af982c3e1551c7
James Preiss <japreiss@usc.edu>
2016-05-30 21:19:53 -0700

    polynomial time stretching function

--- src/modules/src/pptraj.c
+++ src/modules/src/pptraj.c
@@ -46,6 +46,16 @@ static void polyscale(float p[PP_SIZE], float s)
 	}
 }
 
+// e.g. if s==2 the new polynomial will be stretched to take 2x longer
+static void polystretchtime(float p[PP_SIZE], float s)
+{
+	float divisor = s;
+	for (int i = 1; i < PP_SIZE; ++i) {
+		p[i] /= divisor;
+		divisor *= s;
+	}
+}
+
 void poly4d_scale(struct poly4d *p, float x, float y, float z, float yaw)
 {
 	polyscale(p->p[0], x);
