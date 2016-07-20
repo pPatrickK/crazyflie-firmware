000dfe4222c47cf6085ffbc22e2c4c84258011d7
James Preiss <japreiss@usc.edu>
2016-07-19 18:37:25 -0700

    ellipse center fix

--- src/modules/src/ellipsetraj.c
+++ src/modules/src/ellipsetraj.c
@@ -21,7 +21,7 @@ struct traj_eval ellipse_traj_eval(struct ellipse_traj const *e, float t, float 
 	float cos_t = cos(s * t);
 	float sin_t = sin(s * t);
 
-	out.pos  = aubv( cos_t, e->major,  sin(t), e->minor);
+	out.pos  = vadd(aubv( cos_t, e->major,  sin(t), e->minor), e->center);
 	out.vel  = aubv(-s*sin_t, e->major,  s*cos(t), e->minor);
 	struct vec acc  = aubv(-s2*cos_t, e->major, -s2*sin(t), e->minor);
 	struct vec jerk = aubv( s3*sin_t, e->major, -s3*cos(t), e->minor);
