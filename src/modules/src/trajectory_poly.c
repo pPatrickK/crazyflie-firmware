1d47a5ef634ab8360380cf6ca301c8b5a3346f5a
James Preiss <japreiss@usc.edu>
2016-08-10 19:16:18 -0700

    planner state machine to OOP style

--- src/modules/src/trajectory_poly.c
+++ src/modules/src/trajectory_poly.c
@@ -34,44 +34,22 @@
 #include "task.h"
 
 // Crazyswarm includes
-#include "avoidtarget.h"
 #include "crtp.h"
 #include "trajectory.h"
 #include "debug.h"
 #include "mathconstants.h"
 #include "position_external.h"
-#include "pptraj.h"
 #include "packetdef.h"
+#include "planner.h"
 #include "usec_time.h"
 #include "log.h"
 #include "stabilizer.h" // to get current state estimate
 
 
-typedef enum {
-  TRAJECTORY_STATE_IDLE            = 0,
-  TRAJECTORY_STATE_FLYING          = 1,
-  TRAJECTORY_STATE_TAKING_OFF      = 2,
-  TRAJECTORY_STATE_LANDING         = 3,
-  TRAJECTORY_STATE_ELLIPSE         = 4,
-  TRAJECTORY_STATE_ELLIPSE_CATCHUP = 5,
-  TRAJECTORY_STATE_AVOID_TARGET    = 6,
-} trajectoryState_t;
-
 // Global variables
 static bool isInit = false;
 static CRTPPacket p;
-static trajectoryState_t state = TRAJECTORY_STATE_IDLE;
-
-static struct piecewise_traj* ppFront;
-static struct piecewise_traj* ppBack;
-static struct piecewise_traj pp1;
-static struct piecewise_traj pp2;
-
-static struct ellipse_traj ellipse;
-static struct avoid_target avoid;
-
-struct vec home;
-
+static struct planner planner;
 
 // Private functions
 // TODO consistent naming - should they all start with "trajectory" or not?`
@@ -122,8 +100,7 @@ void trajectoryInit(void)
     return;
   }
 
-  ppFront = &pp1;
-  ppBack = &pp2;
+  plan_init(&planner, g_vehicleMass);
 
   //Start the trajectory task
   xTaskCreate(trajectoryTask, TRAJECTORY_TASK_NAME,
@@ -141,71 +118,30 @@ bool trajectoryTest(void)
 
 void trajectoryStop()
 {
-  state = TRAJECTORY_STATE_IDLE;
+  plan_emergency_stop(&planner);
 }
 
 bool trajectoryIsStopped()
 {
-  return state == TRAJECTORY_STATE_IDLE;
+  return plan_is_stopped(&planner);
 }
 
-void pp_eval_to_trajectory_point(struct traj_eval const *ev, trajectoryPoint_t *goal)
-{
-    goal->x = ev->pos.x;
-    goal->y = ev->pos.y;
-    goal->z = ev->pos.z;
-    goal->velocity_x = ev->vel.x;
-    goal->velocity_y = ev->vel.y;
-    goal->velocity_z = ev->vel.z;
-    goal->yaw = ev->yaw;
-    goal->omega = ev->omega;
-}
-
-// works in our structs, not public to rest of firmware
-static struct traj_eval current_goal()
-{
-  float t = usecTimestamp() / 1e6;
-  switch (state) {
-  // NOTE using return instead of break
-  case TRAJECTORY_STATE_ELLIPSE:
-    t = t - ellipse.t_begin;
-    return ellipse_traj_eval(&ellipse, t, g_vehicleMass);
-  case TRAJECTORY_STATE_AVOID_TARGET:
-    return eval_avoid_target(&avoid, t);
-  default:
-    return piecewise_eval(ppFront, t, g_vehicleMass);
-  }
-}
-
-// works in rest of firmware's structs, updates states
 void trajectoryGetCurrentGoal(trajectoryPoint_t* goal)
 {
   float t = usecTimestamp() / 1e6;
-  struct traj_eval ev = current_goal();
-  pp_eval_to_trajectory_point(&ev, goal);
-
-  if (state == TRAJECTORY_STATE_ELLIPSE_CATCHUP) {
-    if (piecewise_is_finished(ppFront, t)) {
-      state = TRAJECTORY_STATE_ELLIPSE;
-    }
+  struct traj_eval ev = plan_current_goal(&planner, t);
+  if (!is_traj_eval_valid(&ev)) {
+    // programming error
+    plan_emergency_stop(&planner);
   }
-  else if (state == TRAJECTORY_STATE_ELLIPSE) {
-    // TODO make it possible for an ellipse to end
-  }
-  else if (state == TRAJECTORY_STATE_AVOID_TARGET) {
-    // TODO anything??
-  }
-  else {
-    // FLYING, TAKING_OFF, or LANDING
-    if (piecewise_is_finished(ppFront, t)) {
-      if (state == TRAJECTORY_STATE_TAKING_OFF) {
-        state = TRAJECTORY_STATE_FLYING;
-      }
-      if (state == TRAJECTORY_STATE_LANDING) {
-        state = TRAJECTORY_STATE_IDLE;
-      }
-    }
-  }
+  goal->x = ev.pos.x;
+  goal->y = ev.pos.y;
+  goal->z = ev.pos.z;
+  goal->velocity_x = ev.vel.x;
+  goal->velocity_y = ev.vel.y;
+  goal->velocity_z = ev.vel.z;
+  goal->yaw = ev.yaw;
+  goal->omega = ev.omega;
 }
 
 void trajectoryTask(void * prm)
@@ -272,29 +208,29 @@ void trajectoryTask(void * prm)
 
 int trajectoryResetPoly(void)
 {
-  ppBack->n_pieces = 0;
+  planner.ppBack->n_pieces = 0;
   return 0;
 }
 
 int trajectoryAddPoly(const struct data_add_poly* data)
 {
   if (data->id < PP_MAX_PIECES
-      && data->offset + data->size < sizeof(ppBack->pieces[data->id])) {
+      && data->offset + data->size < sizeof(planner.ppBack->pieces[data->id])) {
     uint8_t size = data->size;
     uint8_t* ptr = (uint8_t*)&(data->values[0]);
     uint8_t offset = data->offset;
     if (data->offset == 0) {
       // DEBUG_PRINT("setDur: %d %f\n", data->id, data->values[0]);
-      ppBack->pieces[data->id].duration = data->values[0];
+      planner.ppBack->pieces[data->id].duration = data->values[0];
       size -= 1;
       ptr += 4;
     } else {
       offset -= 1;
     }
     for (int i = offset; i < offset + size; ++i) {
-      ppBack->pieces[data->id].p[i/8][i%8] = data->values[offset == 0 ? i+1 : i-offset];
+      planner.ppBack->pieces[data->id].p[i/8][i%8] = data->values[offset == 0 ? i+1 : i-offset];
     }
-    ppBack->n_pieces = data->id + 1;
+    planner.ppBack->n_pieces = data->id + 1;
     // DEBUG_PRINT("trajectoryAdd: %d, %d, %d\n", data->id, offset, size);
     return 0;
   }
@@ -302,139 +238,56 @@ int trajectoryAddPoly(const struct data_add_poly* data)
   return ENOMEM;
 }
 
-void trajectoryFlip()
-{
-  ppBack->t_begin = usecTimestamp() / 1e6;
-  struct piecewise_traj* tmp = ppFront;
-  ppFront = ppBack;
-  ppBack = tmp;
-  // this is necessary because we might recieve the startTrajectory command
-  // repeatedly in a short time... TODO avoid doing this
-  *ppBack = *ppFront;
-}
-
 int trajectoryStartPoly(void)
 {
-  struct traj_eval traj_init = poly4d_eval(&ppBack->pieces[0], 0, g_vehicleMass);
-  struct vec shift_pos = vsub(statePos(), traj_init.pos);
-  piecewise_shift_vec(ppBack, shift_pos, 0);
-
-  trajectoryFlip();
-  // DEBUG_PRINT("trajectory Start\n");
-  // for (int i = 0; i < ppFront->n_pieces; ++i) {
-  //   DEBUG_PRINT("Piece: %d (%f s)\n", i, ppFront->pieces[i].duration);
-  //   for (int j = 0; j < 4; ++j) {
-  //     DEBUG_PRINT("%d: ", j);
-  //     for (int k = 0; k < 8; ++k) {
-  //       DEBUG_PRINT("%f,", ppFront->pieces[i].p[j][k]);
-  //     }
-  //     DEBUG_PRINT("\n");
-  //   }
-  // }
-
+  float t = usecTimestamp() / 1e6;
+  plan_start_poly(&planner, statePos(), t);
   return 0;
 }
 
 int startEllipse(void)
 {
-  ellipse.t_begin = usecTimestamp() / 1e6;
-  struct traj_eval ev_current = current_goal();
-  plan_into_ellipse(&ev_current, &ellipse, ppBack, g_vehicleMass);
-  trajectoryFlip();
-  state = TRAJECTORY_STATE_ELLIPSE_CATCHUP;
+  float t = usecTimestamp() / 1e6;
+  plan_start_ellipse(&planner, t);
   return 0;
-}
-
-static void planTakeoffLanding(float height, float duration)
-{
-  struct vec pos = statePos();
-  float yaw = stateYaw();
-  float delta_z = height - pos.z;
-
-  struct poly4d p = poly4d_takeoff;
-  poly4d_stretchtime(&p, duration / poly4d_takeoff.duration);
-  poly4d_scale(&p, 1, 1, delta_z, 1);
-  poly4d_shift_vec(&p, pos, 0);
-  poly_linear(p.p[3], duration, yaw, 0);
-
-  ppBack->pieces[0] = p;
-  ppBack->n_pieces = 1;
 }
 
 int trajectoryTakeoff(const struct data_takeoff* data)
 {
-  if (state != TRAJECTORY_STATE_IDLE) {
-    return 1;
-  }
-
-  home = statePos();
-  planTakeoffLanding(data->height, data->time_from_start / 1000.0);
-  state = TRAJECTORY_STATE_TAKING_OFF;
-  trajectoryFlip();
-  return 0;
+  float t = usecTimestamp() / 1e6;
+  float duration =  data->time_from_start / 1000.0;
+  return plan_takeoff(&planner, statePos(), stateYaw(), data->height, duration, t);
 }
 
 int trajectoryLand(const struct data_land* data)
 {
-  // TODO this means we can't land from ELLIPSE or AVOID_TARGET
-  // states... you must GO_HOME first. Is this good?
-  if (state != TRAJECTORY_STATE_FLYING) {
-    return 1;
-  }
-
-  planTakeoffLanding(data->height, data->time_from_start / 1000.0);
-  state = TRAJECTORY_STATE_LANDING;
-  trajectoryFlip();
-  return 0;
+  float t = usecTimestamp() / 1e6;
+  float duration =  data->time_from_start / 1000.0;
+  return plan_land(&planner, statePos(), stateYaw(), data->height, duration, t);
 }
 
 int trajectoryHover(const struct data_hover* data)
 {
-  if (state != TRAJECTORY_STATE_FLYING &&
-      state != TRAJECTORY_STATE_ELLIPSE &&
-      state != TRAJECTORY_STATE_AVOID_TARGET) {
-    return 1;
-  }
-
-  struct traj_eval setpoint = current_goal();
+  float t = usecTimestamp() / 1e6;
   struct vec hover_pos = mkvec(data->x, data->y, data->z);
-  float hover_yaw = data->yaw;
-
-  // TODO try out 7th order planner
-  piecewise_plan_5th_order(ppBack, data->duration,
-    setpoint.pos, setpoint.yaw, setpoint.vel, setpoint.omega.z, setpoint.acc,
-    hover_pos,    hover_yaw,    vzero(),      0,                vzero());
-
-  state = TRAJECTORY_STATE_FLYING;
-  trajectoryFlip();
-  return 0;
+  return plan_hover(&planner, hover_pos, data->yaw, data->duration, t);
 }
 
 int goHome(void)
 {
-  struct traj_eval setpoint = current_goal();
-  float distance = vdist(home, setpoint.pos);
-  float duration = 1.0f + distance;
-
-  struct data_hover data;
-  data.x = home.x;
-  data.y = home.y;
-  data.z = home.z;
-  data.yaw = 0;
-  data.duration = duration;
-
-  return trajectoryHover(&data);
+  float t = usecTimestamp() / 1e6;
+  return plan_go_home(&planner, t);
 }
 
 int setEllipse(const struct data_set_ellipse* data)
 {
-  ellipse.center = mkvec_position_fix2float(
+  planner.ellipse.center = mkvec_position_fix2float(
     data->centerx, data->centery, data->centerz);
-  ellipse.major = mkvec_position_fix2float(
+  planner.ellipse.major = mkvec_position_fix2float(
     data->majorx, data->majory, data->majorz);
-  ellipse.minor = mkvec_position_fix2float(
+  planner.ellipse.minor = mkvec_position_fix2float(
     data->minorx, data->minory, data->minorz);
-  ellipse.period = data->period;
+  planner.ellipse.period = data->period;
 
   return 0;
 }
@@ -444,28 +297,22 @@ int startCannedTrajectory(const struct data_start_canned_trajectory* data)
   enum trajectory_type type = data->trajectory;
   switch (type) {
   case TRAJECTORY_FIGURE8:
-    *ppBack = pp_figure8;
+    *planner.ppBack = pp_figure8;
     break;
   default:
     return 1;
   }
 
-  struct traj_eval traj_init = poly4d_eval(&ppBack->pieces[0], 0, g_vehicleMass);
-  struct vec shift_pos = vsub(statePos(), traj_init.pos);
-  piecewise_shift_vec(ppBack, shift_pos, 0);
-  piecewise_stretchtime(ppBack, data->timescale);
-
-  trajectoryFlip();
-
+  float t = usecTimestamp() / 1e6;
+  plan_start_poly(&planner, statePos(), t);
   return 0;
 }
 
 int startAvoidTarget(const struct data_start_avoid_target* data)
 {
-  struct vec home = mkvec(data->x, data->y, data->z);
   float t = usecTimestamp() / 1e6;
-  init_avoid_target(&avoid, home, data->max_speed, data->max_displacement, t);
-  state = TRAJECTORY_STATE_AVOID_TARGET;
+  struct vec home = mkvec(data->x, data->y, data->z);
+  plan_start_avoid_target(&planner, home, data->max_displacement, data->max_speed, t);
   return 0;
 }
 
@@ -473,6 +320,6 @@ int updateAvoidTarget(const struct data_target_position *data)
 {
   float t = usecTimestamp() / 1e6;
   struct vec targetPos = mkvec(data->x, data->y, data->z);
-  update_avoid_target(&avoid, targetPos, t);
+  plan_update_avoid_target(&planner, targetPos, t);
   return 0;
 }
